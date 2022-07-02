c31cf27bf7673bfd93638d86a488b710ac3a6994
Keigo Oka <ogiekako@gmail.com>
2022-07-02 21:42:04 +0900

    New bitboard implementation (bitboard11) complete

--- src/position/bitboard11/rule/reachable.rs
+++ src/position/bitboard11/rule/reachable.rs
@@ -3,7 +3,10 @@ use crate::{
     position::bitboard11::{BitBoard, Square},
 };
 
-use super::power::{lance_power, power};
+use super::{
+    magic,
+    power::{lance_power, power},
+};
 
 pub fn reachable(
     black_pieces: BitBoard,
@@ -22,9 +25,9 @@ pub fn reachable(
 fn reachable_sub(occupied: BitBoard, color: Color, pos: Square, kind: Kind) -> BitBoard {
     match kind {
         Kind::Lance => lance_reachable(occupied, color, pos),
-        Kind::Bishop => bishop_reachable(occupied, pos),
+        Kind::Bishop => magic::bishop_reachable(occupied, pos),
         Kind::Rook => rook_reachable(occupied, pos),
-        Kind::ProBishop => power(color, pos, Kind::King) | bishop_reachable(occupied, pos),
+        Kind::ProBishop => power(color, pos, Kind::King) | magic::bishop_reachable(occupied, pos),
         Kind::ProRook => power(color, pos, Kind::King) | rook_reachable(occupied, pos),
         _ => power(color, pos, kind),
     }
@@ -42,10 +45,218 @@ fn lance_reachable(occupied: BitBoard, color: Color, pos: Square) -> BitBoard {
     })
 }
 
-fn bishop_reachable(occupied: BitBoard, pos: Square) -> BitBoard {
-    todo!()
+fn rook_reachable(occupied: BitBoard, pos: Square) -> BitBoard {
+    lance_reachable(occupied, Color::Black, pos)
+        | lance_reachable(occupied, Color::White, pos)
+        | magic::rook_reachable_row(occupied, pos)
 }
 
-fn rook_reachable(occupied: BitBoard, pos: Square) -> BitBoard {
-    todo!()
+#[cfg(test)]
+mod tests {
+    use crate::{
+        piece::{Color, Kind},
+        position::bitboard11::{testing::bitboard, BitBoard, Square},
+    };
+
+    #[test]
+    fn test_lance_movable_positions() {
+        let occupied = bitboard!(
+            ".........",
+            "......*..",
+            ".........",
+            ".........",
+            "......*..",
+            ".........",
+            ".........",
+            ".........",
+            ".........",
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                "......*..",
+                "......*..",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(2, 3), Kind::Lance)
+        );
+        assert_eq!(
+            bitboard!(
+                "......*..",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(2, 1), Kind::Lance)
+        );
+        assert_eq!(
+            BitBoard::new(),
+            super::reachable_sub(occupied, Color::Black, Square::new(2, 0), Kind::Lance)
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                "......*..",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::White, Square::new(2, 0), Kind::Lance)
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                ".........",
+                "......*..",
+                "......*..",
+                "......*..",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::White, Square::new(2, 1), Kind::Lance)
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                ".........",
+                "......*..",
+                "......*..",
+                "......*..",
+                "......*..",
+            ),
+            super::reachable_sub(occupied, Color::White, Square::new(2, 4), Kind::Lance)
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+            ),
+            super::reachable_sub(occupied, Color::White, Square::new(0, 0), Kind::Lance)
+        );
+        assert_eq!(
+            bitboard!(
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                "........*",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(0, 8), Kind::Lance)
+        );
+    }
+    #[test]
+    fn test_bishop_movable_positions() {
+        let occupied = bitboard!(
+            ".........",
+            "......*..",
+            ".........",
+            ".........",
+            "......*..",
+            "...*.....",
+            "..*......",
+            ".........",
+            ".........",
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                ".......*.",
+                "......*..",
+                ".....*...",
+                "....*....",
+                "...*.....",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(0, 0), Kind::Bishop)
+        );
+        assert_eq!(
+            bitboard!(
+                "......*.*",
+                ".........",
+                "......*.*",
+                ".....*...",
+                "....*....",
+                "...*.....",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(1, 1), Kind::Bishop)
+        );
+        assert_eq!(
+            bitboard!(
+                ".........",
+                "......*.*",
+                ".........",
+                "......*.*",
+                ".....*...",
+                "....*....",
+                "...*.....",
+                "..*......",
+                ".*.......",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(1, 2), Kind::Bishop)
+        );
+    }
+
+    #[test]
+    fn test_rook_movable_positions() {
+        let occupied = bitboard!(
+            ".........",
+            "......*..",
+            ".........",
+            ".........",
+            "......*..",
+            "...*.....",
+            "..*......",
+            ".........",
+            ".........",
+        );
+        assert_eq!(
+            bitboard!(
+                "...*.....",
+                "***.***..",
+                "...*.....",
+                "...*.....",
+                "...*.....",
+                "...*.....",
+                ".........",
+                ".........",
+                ".........",
+            ),
+            super::reachable_sub(occupied, Color::Black, Square::new(5, 1), Kind::Rook)
+        );
+    }
 }
