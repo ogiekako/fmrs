40c5e1613a4de9546c88804c5e1d19ba66c1624c
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 07:05:24 +0000

    add Rust Benchmark (cargo) benchmark result for 6a6806f63f7937b60f3f6054d0974d9e2503d8f4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733467956414,
+  "lastUpdate": 1733468724306,
   "repoUrl": "https://github.com/ogiekako/fmrs",
   "entries": {
     "Rust Benchmark": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
             "name": "oneway",
             "value": 43152,
             "range": "± 163",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ogiekako@gmail.com",
+            "name": "Keigo Oka",
+            "username": "ogiekako"
+          },
+          "committer": {
+            "email": "ogiekako@gmail.com",
+            "name": "Keigo Oka",
+            "username": "ogiekako"
+          },
+          "distinct": true,
+          "id": "6a6806f63f7937b60f3f6054d0974d9e2503d8f4",
+          "message": ".",
+          "timestamp": "2024-12-06T16:01:43+09:00",
+          "tree_id": "c4b60219ca2050911d3f3f22be1825c48a8c1663",
+          "url": "https://github.com/ogiekako/fmrs/commit/6a6806f63f7937b60f3f6054d0974d9e2503d8f4"
+        },
+        "date": 1733468723768,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "black_advance",
+            "value": 4133,
+            "range": "± 304",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "white_advance",
+            "value": 7511,
+            "range": "± 31",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "black_pinned",
+            "value": 580,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "solve3",
+            "value": 4084,
+            "range": "± 36",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "oneway",
+            "value": 42653,
+            "range": "± 191",
             "unit": "ns/iter"
           }
         ]
