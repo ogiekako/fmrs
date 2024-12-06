6cefbec5b906fd5efd5609f6b8b1273bcddc49da
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 06:52:36 +0000

    add Rust Benchmark (cargo) benchmark result for b4bcf70c350a6848108fc1d3988db2d078cce5a0

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1733467956414,
+  "repoUrl": "https://github.com/ogiekako/fmrs",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "ogiekako@gmail.com",
+            "name": "Keigo Oka",
+            "username": "ogiekako"
+          },
+          "committer": {
+            "email": "ogiekako@gmail.com",
+            "name": "Keigo Oka",
+            "username": "ogiekako"
+          },
+          "distinct": true,
+          "id": "b4bcf70c350a6848108fc1d3988db2d078cce5a0",
+          "message": "gh-pages: Keep dev for bench",
+          "timestamp": "2024-12-06T15:48:51+09:00",
+          "tree_id": "0dcac512643c5ed6bcada070d3d43e1be9954a28",
+          "url": "https://github.com/ogiekako/fmrs/commit/b4bcf70c350a6848108fc1d3988db2d078cce5a0"
+        },
+        "date": 1733467955832,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "black_advance",
+            "value": 4126,
+            "range": "± 119",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "white_advance",
+            "value": 7517,
+            "range": "± 51",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "black_pinned",
+            "value": 581,
+            "range": "± 11",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "solve3",
+            "value": 4090,
+            "range": "± 38",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "oneway",
+            "value": 43152,
+            "range": "± 163",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
