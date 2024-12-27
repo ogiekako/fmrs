aed01d2d85c85b03ba105e2bc5de815d28f03047
Keigo Oka <ogiekako@gmail.com>
2024-12-27 09:37:24 +0900

    Revert "update beam not to shift too much"
    This reverts commit f3d40f86f7ba9406becf6c002d6bae45ff6d748f.

--- rust/src/command/one_way_mate/beam.rs
+++ rust/src/command/one_way_mate/beam.rs
@@ -348,7 +348,7 @@ fn random_action(rng: &mut SmallRng, allow_black_capture: bool) -> Action {
                     },
                 )
             }
-            40..=41 => return Action::Shift(rng.gen()), // heavy
+            40..=47 => return Action::Shift(rng.gen()),
             50..=54 => return Action::ChangeTurn,
             60..=60 => return Action::HandToHand(rng.gen(), rng.gen()),
             _ => (),
