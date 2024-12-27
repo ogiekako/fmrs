f2be49edb8f30e9b5a2e184798453bd82a327a39
Keigo Oka <ogiekako@gmail.com>
2024-12-27 09:37:26 +0900

    Revert "use u8 for square"
    This reverts commit a21749b511165d8b047643ab6acb36c362c0864b.

--- rust/fmrs_core/src/position/bitboard/square.rs
+++ rust/fmrs_core/src/position/bitboard/square.rs
@@ -1,32 +1,30 @@
 use rand::{distributions::Standard, prelude::Distribution};
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Square {
-    x: u8,
+    x: usize,
 }
 
 impl Square {
     pub const fn new(col: usize, row: usize) -> Self {
         debug_assert!(col < 9 && row < 9);
-        Self {
-            x: (col * 9 + row) as u8,
-        }
+        Self { x: col * 9 + row }
     }
     pub const fn col(self) -> usize {
-        self.x as usize / 9
+        self.x / 9
     }
     pub const fn row(self) -> usize {
-        self.x as usize % 9
+        self.x % 9
     }
     pub fn iter() -> impl Iterator<Item = Self> {
         (0..81).map(Self::from_index)
     }
     pub(crate) const fn index(self) -> usize {
-        self.x as usize
+        self.x
     }
     pub const fn from_index(x: usize) -> Self {
         debug_assert!(x < 81);
-        Self { x: x as u8 }
+        Self { x }
     }
 
     pub(crate) fn shift(&mut self, dir: crate::direction::Direction) {
