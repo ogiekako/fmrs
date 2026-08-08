88bc4793b445251817c8f1864e92a02041cef0e6
ogiekako <ogiekako@users.noreply.github.com>
2026-08-08 06:12:16 +0000

    Deploying to gh-pages from @ ogiekako/fmrs@3394cc02988e89fe7d1011537c3842504cc66b25 🚀

--- pkg/index_bg.wasm.d.ts
+++ pkg/index_bg.wasm.d.ts
@@ -2,15 +2,15 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const __wbg_backwardsearch_free: (a: number, b: number) => void;
-export const backwardsearch_advance: (a: number) => number;
-export const backwardsearch_new: (a: number, b: number, c: number) => number;
-export const backwardsearch_sfen: (a: number, b: number) => void;
-export const backwardsearch_step: (a: number) => number;
 export const __wbg_get_onewaymateresult_is_one_way: (a: number) => number;
 export const __wbg_get_onewaymateresult_steps: (a: number) => number;
 export const __wbg_onewaymateresult_free: (a: number, b: number) => void;
 export const __wbg_set_onewaymateresult_is_one_way: (a: number, b: number) => void;
 export const __wbg_set_onewaymateresult_steps: (a: number, b: number) => void;
+export const backwardsearch_advance: (a: number) => number;
+export const backwardsearch_new: (a: number, b: number, c: number) => number;
+export const backwardsearch_sfen: (a: number, b: number) => void;
+export const backwardsearch_step: (a: number) => number;
 export const check_one_way_mate: (a: number, b: number) => number;
 export const is_white_in_check: (a: number, b: number) => number;
 export const __wbg_solver_free: (a: number, b: number) => void;
